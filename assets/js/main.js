9f74c993f9fe6a796c7811aa4d8e0f509ef23f76
werthmannsupercode <werthmannsupercode@gmail.com>
2022-04-19 15:04:50 +0200

    first try

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let headline = document.getElementById('headline');
+let list = document.getElementById('list');
+
+function newStyle() {
+    headline.classList.add('headline');
+    list.classList.add('list');
+}
+newStyle();
